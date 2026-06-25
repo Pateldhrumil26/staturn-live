cf0231ce4a6b7e718fca8eed50fb7cf4166df1a4
pateldhrumil26 <dhrumil@gmail.com>
2026-06-25 12:19:29 +0530

    Updated project code

--- frontend/src/admin/AdminDashboard.tsx
+++ frontend/src/admin/AdminDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+
 import API from '../services/api.js';
 import { ResponsiveContainer, AreaChart, Area, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
 import { Layers, Lightbulb, ClipboardCopy, Mail, HeartHandshake, Compass, Users } from 'lucide-react';
